b9bdfc41db8de6b86f8b600ffd77da30f79f509b
Sap <43saps@gmail.com>
2019-11-16 13:19:44 +0530

    Added ID's for TP Link T2U
    RTL8821A

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -329,6 +329,9 @@ static struct usb_device_id rtw_usb_id_tbl[] = {
 	{USB_DEVICE(0x0411, 0x029B),.driver_info = RTL8821}, /* BUFFALO - WI-U2-433DHP */
 	{USB_DEVICE(0x056E, 0x4007),.driver_info = RTL8821}, /* ELECOM - WDC-433DU2H */
 	{USB_DEVICE(0x0BDA, 0xA811),.driver_info = RTL8821}, /* Comfast - CF-915AC, CF-916AC */
+	{USB_DEVICE(0x2357, 0x011E), .driver_info = RTL8821}, /* TP Link - T2U Nano */
+	{USB_DEVICE(0x2357, 0x0122), .driver_info = RTL8821}, /* TP Link - T2U Nano */
+	{USB_DEVICE(0x2357, 0x0120), .driver_info = RTL8821}, /* TP Link - T2U Plus */
 #endif
 
 #ifdef CONFIG_RTL8192E
