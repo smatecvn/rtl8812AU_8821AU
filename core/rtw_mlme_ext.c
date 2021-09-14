39df55967b7de9f6c9600017b724303f95a8b9e2
Ben Greear <greearb@candelatech.com>
2021-09-13 21:04:45 -0700

    Fix compile against 5.15 kernel.
    Better fallthrough syntax, and remove ipx support.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- core/rtw_mlme_ext.c
+++ core/rtw_mlme_ext.c
@@ -757,9 +757,9 @@ void mgt_dispatcher(_adapter *padapter, union recv_frame *precv_frame)
 			ptable->func = &OnAuth;
 		else
 			ptable->func = &OnAuthClient;
-	//falls through
+	fallthrough;
 	case WIFI_ASSOCREQ:
-	// fallthrough
+	fallthrough;
 	case WIFI_REASSOCREQ:
 		_mgt_dispatcher(padapter, ptable, precv_frame);
 #ifdef CONFIG_HOSTAPD_MLME
